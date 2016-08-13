e43bc28f67814e4737572c6525f598d5daf524c5
TastyWaffles <tasty.waffles@outlook.com>
2016-08-13 21:23:59 +0800

    Started work on recursive rename preserving directory structure.

--- ImageHasher/RenameHandler.cs
+++ ImageHasher/RenameHandler.cs
@@ -35,7 +35,7 @@ namespace ImageHasher
           string relevantDirPath = ""; //should be full path of current file from sourceDir down
           string source = ImageUtils.IsDirectory(_options.Source) ? _options.Source : Directory.GetCurrentDirectory();
 
-          //relevantDirPath = file.Directory.FullName.Remove(source);
+
 
           destination = Path.Combine(_options.OutputDir, relevantDirPath, hash + file.Extension);
         }
@@ -61,3 +61,31 @@ namespace ImageHasher
     }
   }
 }
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        CloneDirectory(@"C:\SomeRoot", @"C:\SomeOtherRoot");
+    }
+
+    private static void CloneDirectory(string root, string dest)
+    {
+        foreach (var directory in Directory.GetDirectories(root))
+        {
+            string dirName = Path.GetFileName(directory);
+            if (!Directory.Exists(Path.Combine(dest, dirName)))
+            {
+                Directory.CreateDirectory(Path.Combine(dest, dirName));
+            }
+            CloneDirectory(directory, Path.Combine(dest, dirName));
+        }
+
+        foreach (var file in Directory.GetFiles(root))
+        {
+            File.Copy(file, Path.Combine(dest, Path.GetFileName(file)));
+        }
+    }
+}
+*/
